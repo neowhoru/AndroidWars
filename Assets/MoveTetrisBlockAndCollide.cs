f53b6af48115c92c69f0726481da8a6a1a8ec7ae
alexander <alexander.carparelli@atino.de>
2017-01-13 11:11:27 +0100

    added a scene like tricky towers game for learning propose

---
+++ Assets/MoveTetrisBlockAndCollide.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTetrisBlockAndCollide : MonoBehaviour {
+
+    public float speed = 0.5f;
+    private bool isMoveableByUser = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (isMoveableByUser)
+	    {
+
+	        if (Input.GetKeyDown(KeyCode.RightArrow))
+	        {
+	            transform.position = new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z);
+	        }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.position = new Vector3(transform.position.x - 0.5f, transform.position.y , transform.position.z);
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Debug.Log("LEFT ROTATE");
+
+               
+                transform.Rotate(Vector3.forward * -90);
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Debug.Log("RIGHT ROTATE");
+                transform.Rotate(Vector3.forward * 90);
+            }
+        }
+	    
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "ground" || coll.gameObject.tag == "block")
+        {
+            isMoveableByUser = false;
+        }
+            
+
+    }
+}
---
+++ Assets/SpawnRandomBlocks.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnRandomBlocks : MonoBehaviour {
+
+    public GameObject LBlockPrefab;
+    public GameObject LBlockReversedPrefab;
+    public GameObject ZBlockPrefab;
+    public GameObject ZBlockReversedPrefab;
+    public GameObject LongBlockPrefab;
+    public GameObject FourBlockPrefab;
+    // Use this for initialization
+    public bool isGameRunning = false;
+    void Start () {
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isGameRunning && Input.GetMouseButtonDown(0))
+        {
+            isGameRunning = true;
+            InvokeRepeating("spawnRandomBlock",1.0f,2.0f);
+
+        }
+    }
+
+    void spawnRandomBlock()
+    {
+        int randomBlock = Random.Range(0, 5);
+
+        GameObject theBlock;
+        switch (randomBlock)
+        {
+            case 0:
+                theBlock = Instantiate(LBlockPrefab);
+                break;
+            case 1:
+                theBlock = Instantiate(LBlockReversedPrefab);
+                break;
+            case 2:
+                theBlock = Instantiate(ZBlockPrefab);
+                break;
+            case 3:
+                theBlock = Instantiate(ZBlockReversedPrefab);
+                break;
+            case 4:
+                theBlock = Instantiate(LBlockPrefab);
+                break;
+            case 5:
+                theBlock = Instantiate(FourBlockPrefab);
+                break;
+
+
+        }
+    }
+}
